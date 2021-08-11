f45e514572d5a8d06f123ed98d4dd3d1b31b5259
Svenden123 <buhrus1@yandex.ru>
2021-08-11 19:20:13 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#hungry = False
+
+#if hungry:
+    #print('FEED ME!')
+#else:
+    #print("I'm not hungry")
+
+
+
+
+
+#hungry = True
+
+#if hungry:
+    #print('FEED ME!')
+#else:
+    #print("I'm not hungry")
+
+
+
+
+
+#loc = "BANK"
+
+#if loc == 'Auto Shop':
+    #print("Cars are cool!")
+#else:
+    #print("I don't know much")
+
+
+#loc = "BANK"
+
+#if loc == 'Auto Shop':
+    #print("Cars are cool!")
+#elif loc == "BANK":
+    #print("Money is cool")
+#else:
+    #print("I don't know much")
+
+
+loc = "Game"
+
+if loc == "Auto Shop":
+    print("Cars are cool!")
+elif loc == "Bank":
+    print("Money is cool")
+elif loc == "Store":
+    print("Welcome to store")
+else:
+    print("I do not how much")
+
+
